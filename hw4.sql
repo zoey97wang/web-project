6aa5b84c082901801e6b69335ec50e4220008caa
zoeywang <zwang297@gmail.com>
2016-12-07 19:19:00 -0500

    new table

--- hw4.sql
+++ hw4.sql
@@ -1,8 +1,10 @@
 create table guestbook(
 name varchar(20),
 email varchar(30),
-comments varchar(30)
+comments varchar(30),
+creation_date date
 )
 
-
+alter table guestbook add(phoneNumber varchar(20))
+alter table guestbook add(gender varchar(20))
 select * from guestbook
